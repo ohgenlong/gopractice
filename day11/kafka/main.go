2f4dd15dec7dcaaa4b5a61fdda317f3e1856bf36
longzhijun <ohgenlong@126.com>
2018-05-28 11:35:06 +0800

    update

--- day11/kafka/main.go
+++ day11/kafka/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"time"
 	"fmt"
 	"github.com/Shopify/sarama"
 )
@@ -18,18 +19,19 @@ func main() {
 		return
 	}
 	defer client.Close()
+	for {
+		msg := &sarama.ProducerMessage{}
+		msg.Topic = "nginx_log"
+		msg.Value = sarama.StringEncoder("this is a good test, msg is good")
 
-	msg := &sarama.ProducerMessage{}
-	msg.Topic = "test"
-	msg.Value = sarama.StringEncoder("this is a good test, msg is good")
+		pid, offset, err := client.SendMessage(msg)
 
-	pid, offset, err := client.SendMessage(msg)
-
-	if err != nil {
-		fmt.Println("send msg failed: ", err)
-		return
+		if err != nil {
+			fmt.Println("send msg failed: ", err)
+			return
+		}
+		fmt.Printf("pid: %v, offset:%v\n", pid, offset)
+		time.Sleep(10*time.Millisecond)
 	}
-
-	fmt.Printf("pid: %v, offset:%v\n", pid, offset)
-
+	
 }
